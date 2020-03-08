da10e9f38c8ac682f76886f14d60671245061fef
Niels van Velzen <git@ndat.nl>
2020-03-08 17:55:38 +0100

    Update to Jellyfin 10.5.0-pre1

--- publish.ps1
+++ publish.ps1
@@ -1,3 +1,3 @@
 dotnet build
-Copy-Item ".\bin\Debug\netcoreapp2.0\Jellyfin.Plugin.Resolver.dll" -Destination "C:\ProgramData\Jellyfin\Server\plugins"
-Copy-Item ".\bin\Debug\netcoreapp2.0\Jellyfin.Plugin.Resolver.pdb" -Destination "C:\ProgramData\Jellyfin\Server\plugins"
+Copy-Item ".\bin\Debug\netcoreapp3.1\Jellyfin.Plugin.Resolver.dll" -Destination "C:\ProgramData\Jellyfin\Server\plugins"
+Copy-Item ".\bin\Debug\netcoreapp3.1\Jellyfin.Plugin.Resolver.pdb" -Destination "C:\ProgramData\Jellyfin\Server\plugins"
