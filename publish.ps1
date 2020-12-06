5b1c010640e1e0b95aebfda32ca03d6b46c8d4a9
Niels van Velzen <git@ndat.nl>
2020-12-06 12:39:32 +0100

    Fix publishing script

--- publish.ps1
+++ publish.ps1
@@ -1,6 +1,6 @@
 dotnet build
-Copy-Item ".\bin\Debug\netstandard2.1\Jellyfin.Plugin.Resolver.dll" -Destination "C:\ProgramData\Jellyfin\Server\plugins"
-Copy-Item ".\bin\Debug\netstandard2.1\Jellyfin.Plugin.Resolver.pdb" -Destination "C:\ProgramData\Jellyfin\Server\plugins"
+Copy-Item ".\bin\Debug\net5.0\Jellyfin.Plugin.Resolver.dll" -Destination "C:\ProgramData\Jellyfin\Server\plugins"
+Copy-Item ".\bin\Debug\net5.0\Jellyfin.Plugin.Resolver.pdb" -Destination "C:\ProgramData\Jellyfin\Server\plugins"
 
-Copy-Item ".\bin\Debug\netstandard2.1\AnitomySharp.dll" -Destination "C:\ProgramData\Jellyfin\Server\plugins"
-Copy-Item ".\bin\Debug\netstandard2.1\AnitomySharp.pdb" -Destination "C:\ProgramData\Jellyfin\Server\plugins"
+Copy-Item ".\bin\Debug\net5.0\AnitomySharp.dll" -Destination "C:\ProgramData\Jellyfin\Server\plugins"
+Copy-Item ".\bin\Debug\net5.0\AnitomySharp.pdb" -Destination "C:\ProgramData\Jellyfin\Server\plugins"
