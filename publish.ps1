71da43168a1ef9bea057f479b5d0392fdd7a44bb
Niels van Velzen <git@ndat.nl>
2022-03-29 13:24:15 +0200

    Update to .NET6/JF10.8 beta 1

--- publish.ps1
+++ publish.ps1
@@ -1,6 +1,6 @@
 dotnet publish
 New-Item -ItemType Directory -Force -Path "C:\ProgramData\Jellyfin\Server\plugins\Resolver_local\"
-Copy-Item ".\bin\Debug\net5.0\Jellyfin.Plugin.Resolver.dll" -Destination "C:\ProgramData\Jellyfin\Server\plugins\Resolver_local\"
-Copy-Item ".\bin\Debug\net5.0\Jellyfin.Plugin.Resolver.pdb" -Destination "C:\ProgramData\Jellyfin\Server\plugins\Resolver_local\"
-Copy-Item ".\bin\Debug\net5.0\AnitomySharp.dll" -Destination "C:\ProgramData\Jellyfin\Server\plugins\Resolver_local\"
-Copy-Item ".\bin\Debug\net5.0\AnitomySharp.pdb" -Destination "C:\ProgramData\Jellyfin\Server\plugins\Resolver_local\"
+Copy-Item ".\bin\Debug\net6.0\Jellyfin.Plugin.Resolver.dll" -Destination "C:\ProgramData\Jellyfin\Server\plugins\Resolver_local\"
+Copy-Item ".\bin\Debug\net6.0\Jellyfin.Plugin.Resolver.pdb" -Destination "C:\ProgramData\Jellyfin\Server\plugins\Resolver_local\"
+Copy-Item ".\bin\Debug\net6.0\AnitomySharp.dll" -Destination "C:\ProgramData\Jellyfin\Server\plugins\Resolver_local\"
+Copy-Item ".\bin\Debug\net6.0\AnitomySharp.pdb" -Destination "C:\ProgramData\Jellyfin\Server\plugins\Resolver_local\"
