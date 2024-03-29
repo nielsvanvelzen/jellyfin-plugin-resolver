3b1bcfba2082824e5dc60e1f915d38ed1f5c5d42
Niels van Velzen <git@ndat.nl>
2024-03-29 15:49:20 +0100

    Update publish.ps1

--- publish.ps1
+++ publish.ps1
@@ -1,5 +1,9 @@
 dotnet publish
-New-Item -ItemType Directory -Force -Path "C:\ProgramData\Jellyfin\Server\plugins\Resolver_local\"
-Copy-Item ".\bin\Release\net8.0\publish\Jellyfin.Plugin.Resolver.dll" -Destination "C:\ProgramData\Jellyfin\Server\plugins\Resolver_local\"
-Copy-Item ".\bin\Release\net8.0\publish\Jellyfin.Plugin.Resolver.pdb" -Destination "C:\ProgramData\Jellyfin\Server\plugins\Resolver_local\"
-Copy-Item ".\bin\Release\net8.0\publish\AnitomySharp.dll" -Destination "C:\ProgramData\Jellyfin\Server\plugins\Resolver_local\"
+# Windows Tray
+#$jellyfin = "C:\ProgramData\Jellyfin\Server"
+# dotnet run
+$jellyfin = "C:\Users\Niels\AppData\Local\jellyfin"
+New-Item -ItemType Directory -Force -Path "$jellyfin\plugins\Resolver_local\"
+Copy-Item ".\bin\Release\net8.0\publish\Jellyfin.Plugin.Resolver.dll" -Destination "$jellyfin\plugins\Resolver_local\"
+Copy-Item ".\bin\Release\net8.0\publish\Jellyfin.Plugin.Resolver.pdb" -Destination "$jellyfin\plugins\Resolver_local\"
+Copy-Item ".\bin\Release\net8.0\publish\AnitomySharp.dll" -Destination "$jellyfin\plugins\Resolver_local\"
